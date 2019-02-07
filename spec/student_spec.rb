2f6705820c1c8a638e9b2754cc79f5c3640493c5
Dakota Clark <bannaray95@gmail.com>
2019-02-07 19:04:18 +0000

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,21 +1,6 @@
 require_relative 'spec_helper'
 
 describe Student do
-  before :each do
-    DB[:conn].execute("DROP TABLE IF EXISTS students")
-
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
-      grade INTEGER
-      )
-    SQL
-
-    DB[:conn].execute(sql)
-    DB[:conn].results_as_hash = true
-  end
-
   let(:attributes) {
     {
       id: nil,
@@ -25,12 +10,6 @@ describe Student do
   }
 
   let(:new_student) {Student.new(attributes)}
-
-  describe 'inheritance' do
-    it 'inherits from InteractiveRecord class' do
-      expect(Student).to be < InteractiveRecord 
-    end
-  end
 
   describe '.table_name' do 
     it 'creates a downcased, plural table name based on the Class name' do 
@@ -85,7 +64,7 @@ describe Student do
     end
 
     describe '#values_for_insert' do 
-      it 'formats the column names to be used in a SQL statement' do 
+      it 'formats the column names to be used in s SQL statement' do 
         expect(new_student.values_for_insert).to eq("'Sam', '11'")
       end
     end
@@ -93,12 +72,12 @@ describe Student do
     describe '#save' do 
       it 'saves the student to the db' do 
         new_student.save
-        expect(DB[:conn].execute("SELECT * FROM students WHERE name = 'Sam'")).to eq([{"id"=>1, "name"=>"Sam", "grade"=>11, 0=>1, 1=>"Sam", 2=>11}])
+        expect(DB[:conn].execute("SELECT * FROM students WHERE name = 'Sam'")).to eq([{"id"=>1, "name"=>"Sam", "grade"=>11}])
       end
 
       it 'sets the student\'s id' do
         new_student.save
-        expect(new_student.id).to eq(1)
+        expect(new_student.id).to eq(2)
       end
     end
   end
@@ -106,20 +85,7 @@ describe Student do
   describe '.find_by_name' do 
     it 'executes the SQL to find a row by name' do 
       Student.new({name: "Jan", grade: 10}).save
-      expect(Student.find_by_name("Jan")).to eq([{"id"=>1, "name"=>"Jan", "grade"=>10, 0=>1, 1=>"Jan", 2=>10}])
-    end
-  end
-
-  describe '.find_by' do 
-    it 'executes the SQL to find a row by the attribute passed into the method' do 
-      Student.new({name: "Susan", grade: 10}).save
-      expect(Student.find_by({name: "Susan"})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
-    end
-
-    it 'accounts for when an attribute value is an integer' do
-      Student.new({name: "Susan", grade: 10}).save
-      Student.new({name: "Geraldine", grade: 9}).save
-      expect(Student.find_by({grade: 10})).to eq([{"id"=>1, "name"=>"Susan", "grade"=>10, 0=>1, 1=>"Susan", 2=>10}])
+      expect(Student.find_by_name("Jan")).to eq([{"id"=>3, "name"=>"Jan", "grade"=>10}])
     end
   end
 end
